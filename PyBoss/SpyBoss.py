f83e755325b5556af4571e8795019c598557e5bf
Mikegoldster <mikegoldster@gmail.com>
2019-03-02 21:22:06 -0600

    final commit

--- PyBoss/SpyBoss.py
+++ PyBoss/SpyBoss.py
@@ -9,23 +9,39 @@ Created on Thu Feb 28 12:57:13 2019
 import pandas as pd
 import os
 import csv 
-df = pd.read_csv(r'C:\Users\mikeg\OneDrive\Desktop\RiceDataCamp\GitClone\python-challenge\PyBoss\employee_data.csv', delimiter = ",")
+
+#Import the employee_data1.csv and employee_data2.csv 
+
+df = pd.read_csv(r'C:\Users\mikeg\OneDrive\Desktop\RiceDataCamp\GitClone\python-challenge\PyBoss\employee_data.csv')
 
 #The Name column should be split into separate First Name and Last Name columns.
 # new data frame with split value columns 
-new = df["Name"].str.split(" ", n = 1, expand = True)  
+new = df["Name"].str.split(" ", n = 1, expand = True) 
+ 
 # making seperate first name column from new data frame 
 df["First Name"]= new[0] 
+
 # making seperate last name column from new data frame 
 df["Last Name"]= new[1] 
+
 # Dropping old Name columns 
 df.drop(columns =["Name"], inplace = True) 
 
 #The DOB data should be re-written into MM/DD/YYYY format.
-
+df['DOB'] = pd.to_datetime(df['DOB']).dt.strftime('%m/%d/%Y')
 
 #The SSN data should be re-written such that the first five numbers are hidden from view.
-
+df['SSN'] = df['SSN'].str.replace(r'^\d{3}-\d{2}', "***-**", regex=True)
 
 #The State data should be re-written as simple two-letter abbreviations.
-
+us_state_abbrev = {
+'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO',
+'Connecticut': 'CT', 'Delaware': 'DE', 'Florida': 'FL', 'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID',
+'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA',
+'Maine': 'ME', 'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS',
+'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ',
+'New Mexico': 'NM', 'New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH', 'Oklahoma': 'OK',
+'Oregon': 'OR', 'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina': 'SC', 'South Dakota': 'SD',
+'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT', 'Virginia': 'VA', 'Washington': 'WA',
+'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'}
+df['State'] = df['State'].map(us_state_abbrev).fillna(df['State'])
--- PyPoll/SpyPoll.py
+++ PyPoll/SpyPoll.py
@@ -7,35 +7,44 @@ Created on Thu Feb 28 10:11:11 2019
 
 #import modules
 import pandas as pd
+import numpy as np
 import os
 import csv 
 df = pd.read_csv(r'C:\Users\mikeg\OneDrive\Desktop\RiceDataCamp\GitClone\python-challenge\PyPoll\election_data.csv', delimiter = ",")
 
 #The total number of votes cast
 TotalVotes = df['Voter ID'].count()
-print(TotalVotes)
 
 #A complete list of candidates who received votes
 UniqueCands = df['Candidate'].unique()
-print(UniqueCands)
 
-#The total number of votes each candidate won
-candidatewin = df['Candidate'].value_counts()
-Election_df = pd.DataFrame(candidatewin)
+#Total votes won by each candidate
+candidate_counts = df['Candidate'].value_counts()
 
-Election_df = Election_df.rename(
-    columns={"Index": "Name", "Candidate": "Votes"})
-Election_df.head()
-
-#Change new candidatewin dataframe column name - FAIL
-#candidatewin.rename({1: 'Candidate', 2: 'Votes'}, axis='columns')
-#candidatewin['total'] = candidatewin.sum(axis=0)
-Election_df['Percentage'] = (((df['Candidate'].value_counts())/TotalVotes)*100).round(2).astype(str) + '%'
+#The percentage of votes each candidate won
+candidate_totals = pd.DataFrame({'Candidate': UniqueCands, 'Votes': candidate_counts})
+candidate_totals['Votes'] = pd.to_numeric(candidate_totals['Votes'])
+candidate_totals['Percentage'] = (((candidate_totals['Votes'])/TotalVotes)*100).round(2).astype(str) + '%'
 
 
 #The winner of the election based on popular vote.
-WinnerCand = Election_df.loc[Election_df['Percentage'].max(), 'Index']
-print(WinnerCand)
+WinnerCand = candidate_totals['Votes'].max()
+Winner = candidate_totals.loc[candidate_totals["Votes"] == WinnerCand]
 
+#Set index to Candidate..
+finalcand = candidate_totals.set_index("Candidate")
+
+#The winner of the election based on popular vote.
+WinnerCand = candidate_totals.loc[candidate_totals['Votes'].idxmax(), 'Candidate']
 
 #Summary of Election Data
+print("Election Results")
+print("-------------------------")
+print("Total Votes: ")
+print(TotalVotes)
+print("-------------------------")
+print(finalcand)
+print("-------------------------") 
+print("Winner: ")
+print(WinnerCand)
+print("-------------------------")
